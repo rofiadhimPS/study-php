b7c06d142f8e1597ce9faa11e21ed170794c66d3
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-05 19:37:25 +0700

    number data type (int)

---
+++ NumberDataType.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(0123);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b111111);
+
+echo "Underscore in number : ";
+var_dump(1_123_412_341);
