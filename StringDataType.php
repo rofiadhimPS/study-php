fd6e027ed50fb44541c3e9e6ed1f7e9a5fcd2906
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-09 13:10:54 +0700

    working with heredoc and nowdoc

--- StringDataType.php
+++ StringDataType.php
@@ -6,3 +6,20 @@ echo "\n";
 
 echo "Name : ";
 echo "Muh\t Rofiadhim\t Rajab";
+
+// heredoc can parsing variable
+echo <<<ADHIM
+    \n
+    This is multiline teks 
+    for example, now we study string data type
+    can using heredoc
+    \n
+ADHIM;
+
+// nowdoc
+echo <<<'ADHIM'
+    This is multiline teks 
+    for example, now we study string data type
+    can using nowdoc, can't parsing variable
+ADHIM;
+
