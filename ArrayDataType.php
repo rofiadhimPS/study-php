3faab3f27309266616a400317a15dc68eccc6bcc
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-17 20:19:27 +0700

    array as a map

--- ArrayDataType.php
+++ ArrayDataType.php
@@ -18,3 +18,36 @@ $names[] = "Bily";
 var_dump($names);
 
 var_dump(count($names));
+
+// Array as a Map
+
+$adhim = array (
+    "id" => "adhim",
+    "name" => "Rofiadhim Rajab",
+    "age" => 22,
+    "address" => [
+        "city" => "Makassar",
+        "country" => "Indoensia",
+    ],
+);
+var_dump($adhim);
+
+var_dump($adhim["name"]);
+var_dump($adhim["age"]);
+var_dump($adhim["address"]["country"]);
+
+$rajab = [
+    "id" => "rajab",
+    "name" => "Abdul Rajab",
+    "age" => 50,
+    "address" => array (
+        "city" => "Makassar",
+        "country" => "Indoensia",
+    ),
+];
+var_dump($rajab);
+
+var_dump($rajab["address"]["city"]);
+
+
+
