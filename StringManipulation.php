8a4f3f3d76d6aa99ab272edd91159d3b4d0b3dc9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-19 15:47:18 +0700

    string manipulation - datatype conversion

--- StringManipulation.php
+++ StringManipulation.php
@@ -3,3 +3,17 @@
 $name = "Muh Rofiadhim Rajab";
 
 echo "Name : " . $name . PHP_EOL;
+echo "Value : " . 100 . PHP_EOL;
+
+//datatype conversion
+
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)"1.01";
+var_dump($valueFloat);
+
+
