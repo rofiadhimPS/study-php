0518bc8a88ebb79f8aa86f9ce8ca70b229090867
rofiadhimPS <muhammadrofiadhim@gmail.com>
2024-12-20 20:29:47 +0700

    working with string function

---
+++ StringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(join(",", [10,13,12,4,15]));
+var_dump(explode(" ", "Muh Rofiadhim Rajab"));
+var_dump(strtolower("ADHIM"));
+var_dump(strtoupper("adhim"));
+var_dump(trim("   adhim     hello    "));
+var_dump(substr("Muh Rofiadhim Rajab", 0, 3));
