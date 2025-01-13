4a74b13c2e3b15b054bd629aa4eff213562857c6
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 13:36:51 +0700

    working with function

--- data/Person.php
+++ data/Person.php
@@ -5,4 +5,8 @@ class Person
     var string $name;
     var ?string $address = null; // nullable
     var string $country = "Indonesia"; // set default value
+
+    function sayHello(string $name){
+        echo "Hello $name" . PHP_EOL;
+    }
 }
