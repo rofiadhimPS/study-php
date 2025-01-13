40ce62af4f320aeeb9f9dbbf9a7cf867f7931fd9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 20:22:41 +0700

    abstract class

---
+++ AbstractClass.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "data/Location.php";
+
+use Data\{Location, City, Province, Country};
+
+// $location = new Location();  ERROR
+
+$city = new City();
+$province = new Province();
+$country = new Country();
+
