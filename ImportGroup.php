5e361b7ddae977bc8863e07baa3b88119aaf9499
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 16:46:59 +0700

    working with import group

---
+++ ImportGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "data/Conflict.php";
+require_once "data/Helper.php";
+
+use Data\One\{Conflict as Conflict1, Sample, Dummy};
+use function Helper\{helpMe};
+
+$conflict = new Conflict1();
+$dummy = new Dummy();
+$sample = new Sample();
+
