48cf79e1c9a59ef1d10e0b242c49abc0f1075fd2
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-13 13:42:06 +0700

    working with "this" keyword

--- Function.php
+++ Function.php
@@ -2,6 +2,13 @@
 
 require_once "data/Person.php";
 
-$person = new Person();
-$person->sayHello("Adhim");
+$adhim = new Person();
+$adhim->name = "Adhim";
+
+$adhim->sayHello("Muh");
+
+$rajab = new Person();
+$rajab->name = "Rajab";
+
+$rajab->sayHello(null);
 
--- data/Person.php
+++ data/Person.php
@@ -6,7 +6,11 @@ class Person
     var ?string $address = null; // nullable
     var string $country = "Indonesia"; // set default value
 
-    function sayHello(string $name){
-        echo "Hello $name" . PHP_EOL;
+    function sayHello(?string $name){
+        if(is_null($name)){
+            echo "Hi, my name $this->name" . PHP_EOL;
+        }else{
+            echo "Hi $name, my name $this->name" . PHP_EOL;
+        }
     }
 }
