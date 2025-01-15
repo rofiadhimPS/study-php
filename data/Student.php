51616058f003655862282c8ad1226e1e559de412
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-15 09:55:19 +0700

    working with magic function

--- data/Student.php
+++ data/Student.php
@@ -15,4 +15,27 @@ class Student
     {
         unset($this->sample);
     }
+
+    public function __toString()
+    {
+        return "Student id:$this->id, name:$this->name, value:$this->value";
+    }
+
+    public function __invoke(...$arguments)
+    {
+        $join = join(",", $arguments);
+        echo "Invoke student with arguments $join" . PHP_EOL;
+    }
+
+    public function __debugInfo()
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "value" => $this->value,
+            "sample" => $this->sample,
+            "author" => "Adhim",
+            "version" => "1.0.0",
+        ];
+    }
 }
