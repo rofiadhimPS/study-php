262fe99a89a45b195e05486db7402de94dbba744
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-15 07:38:06 +0700

    interface inheritance

--- data/Car.php
+++ data/Car.php
@@ -2,15 +2,34 @@
 
 namespace Data;
 
-interface Car
+interface HasBrand
+{
+    function getBrand(): string;
+}
+
+interface IsMaintenance
+{
+    function IsMaintenance(): bool;
+}
+
+interface Car extends HasBrand
 {
     function drive(): void;
 
     function getTire(): int;
 }
 
-class Avanza implements Car
+class Avanza implements Car, IsMaintenance
 {
+    public function getBrand(): string
+    {
+        return "Toyota" . PHP_EOL;
+    }
+
+    public function IsMaintenance(): bool
+    {
+        return false;
+    }
     public function drive(): void
     {
         echo "Drive Avanza" . PHP_EOL;
