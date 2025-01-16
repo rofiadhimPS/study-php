019b043589817f0684e423a498ff7eae99f02c48
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-16 14:22:28 +0700

    non capturing catches

---
+++ NonCapturingCatches.php
@@ -0,0 +1,13 @@
+<?php
+
+function validate(string $value){
+    if (trim($value) == "") {
+        throw new Exception("Invalid string");
+    }
+}
+
+try{
+    validate("   ");
+}catch(Exception){
+    echo "Invalid" . PHP_EOL;
+}
